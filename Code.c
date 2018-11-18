42fee78f258f0b442f88207f309350651104a957
ayuzva <andrikyuzva@gmail.com>
2018-11-18 04:47:28 +0000

    fuck this

---
+++ Code.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#include <stdint.h>
+
+#include <wiringPi.h>
+#include <wiringSerial.h>
+
+int main()
+{
+	int serial_port;
+	int xcoord;
+	int ycoord;
+	char payload;
+
+	if((serial_port = serialOpen("/dev/ttyAMA0", 115200)) < 0)
+	{
+		fprintf(stderr, "Unable to open serial device: %s\n", strerror(errno));
+		return 1;
+	}
+
+	if(wiringPiSetup() == -1)
+	{
+		fprintf(stdout, "Unable to start wiringPi: %s\n", strerror(errno));
+		return 1;
+	}
+
+	while(1){
+			printf("Sending Coordinate...\n");
+
+			xcoord = 250;
+			ycoord = 0;
+
+			serialPutchar(serial_port, 'm');
+
+			payload = (xcoord >> (8*0)) & 0xff;
+			serialPutchar(serial_port, payload);
+
+			payload = (xcoord >> (8*1)) & 0xff;
+			serialPutchar(serial_port, payload);
+
+			payload = (ycoord >> (8*0)) & 0xff;
+			serialPutchar(serial_port, payload);
+
+			payload = (ycoord >> (8*1)) & 0xff;
+			serialPutchar(serial_port, payload);
+
+			serialPutchar(serial_port, 'm');
+
+			exit(0);
+}
+
+exit(0);
+}
