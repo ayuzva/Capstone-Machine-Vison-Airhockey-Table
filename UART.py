fce39c5e50c82f087af47479c2dba8d15923695b
ayuzva <42620644+ayuzva@users.noreply.github.com>
2018-11-23 12:15:38 -0500

    Rewrote it during lunch

---
+++ UART.py
@@ -0,0 +1,14 @@
+import serial
+
+ser = serial.Serial ("/dev/ttyS0", 1000000)    #Open port with baud rate
+while True:
+	ser.write("m")
+			payload = (xcoord >> (8*0)) & 0xff
+			ser.write(payload)
+			payload = (xcoord >> (8*1)) & 0xff
+			ser.write(payload)
+			payload = (ycoord >> (8*0)) & 0xff
+			ser.write(payload)
+			payload = (ycoord >> (8*1)) & 0xff
+			ser.write(payload)
+	ser.write("m")
